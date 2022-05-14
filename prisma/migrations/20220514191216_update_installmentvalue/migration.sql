f100659fe5c24e5229af952bd17a8b92eada854f
Guilherme <guilherme.rodriguesdasilva1010@gmail.com>
2022-05-14 18:06:23 -0300

    Alterado as migrações

--- prisma/migrations/20220514191216_update_installmentvalue/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - You are about to alter the column `installmentValue` on the `Purchases` table. The data in that column could be lost. The data in that column will be cast from `Int` to `Double`.
-
-*/
--- AlterTable
-ALTER TABLE `Purchases` MODIFY `installmentValue` DOUBLE NOT NULL;
--- prisma/migrations/20220514192731_update_purchases/migration.sql
+++
@@ -1,9 +0,0 @@
-/*
-  Warnings:
-
-  - You are about to drop the column `limit` on the `Purchases` table. All the data in the column will be lost.
-
-*/
--- AlterTable
-ALTER TABLE `Purchases` DROP COLUMN `limit`,
-    MODIFY `startDate` VARCHAR(191) NOT NULL;
