cf5eefb58440db59b52dbf17ddb447cbd0ee4546
Guilherme <guilherme.rodriguesdasilva1010@gmail.com>
2022-05-14 17:13:27 -0300

    Criação da rota padrão

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,10 +3,12 @@ import { ConfigModule } from '@nestjs/config';
 import { CardModule } from './card/card.module';
 import { PrismaService } from './prisma.service';
 import { ExpensesModule } from './expenses/expenses.module';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
 
 @Module({
   imports: [ConfigModule.forRoot(),CardModule, ExpensesModule],
-  // controllers: [AppController],
-  providers: [PrismaService],
+  controllers: [AppController],
+  providers: [PrismaService, AppService],
 })
 export class AppModule {}
